7851104534e461577fa678b6f01b794ff072bdce
Patrick Brooks <patrick.n.brooks@gmail.com>
2019-03-02 17:30:35 -0500

    Added login and user db

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
-from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from app.models import User
+from flask import flash, redirect, request, render_template, url_for
+from flask_login import current_user, login_required, login_user, logout_user
+from werkzeug.urls import url_parse
+
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    usermock = {'username': 'Patrick'}
     postsmock = [
         {
             'author': {'username': 'John'},
@@ -16,13 +20,38 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=usermock, posts=postsmock)
+    return render_template('index.html', title='Home', posts=postsmock)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # if current user is already logged in, then don't log in again
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        # user exists and password is correct
+        login_user(user, remember=form.remember_me.data)
+
+        # if user came from a local page, then return them to that
+        # page after authentication ... else go to /index
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+
+    # GET just renders the empty login screen
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
--- outpost-calc-web.py
+++ outpost-calc-web.py
@@ -1,3 +1,8 @@
 # from the app package (i.e. subdir), import the app variable
 # which is a Flask(__name__)
-from app import app
+from app import app, db
+from app.models import User
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
